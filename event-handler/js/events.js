edc39488559979ba9c1a1fe1fab180258dade2d7
hadimdnor <hadimdnor21@gmail.com>
2021-10-01 20:39:11 +0800

    addedd password code

--- event-handler/js/events.js
+++ event-handler/js/events.js
@@ -6,3 +6,16 @@ let elNamaPengguna = document.getElementById("namaPengguna")
 elNamaPengguna.addEventListener("keyup", function(){
     elNamaPengguna.value = transformUpperCase(elNamaPengguna.value)
 })
+
+function checkPassword(){
+    let elKataLaluan = document.getElementById("kataLaluan")
+    let pass_val = elKataLaluan.value
+    if(pass_val.length > 5){
+        alert("password length accepted")
+    } else {
+        alert("password too short - make sure it 5 or more words")
+    }
+}
+
+let elPassword = document.getElementById("kataLaluan")
+elPassword.onblur = checkPassword
